28e4924b938e889f59199f648b77adfb7acafea2
Vichon Jward <vichonjward@gmail.com>
2020-11-01 15:59:10 -0800

    50 pulls from page

--- response.py
+++ response.py
@@ -1,11 +1,43 @@
+from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "https://www.indeed.com/jobs?q=python&l=Dallas%2C+TX&radius=35"
+# html url 
+url = "https://www.indeed.com/jobs?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=0&l=Sacramento%2C+CA&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch"
+
+# pull data from html page
 response = requests.get(url)
 
-outp = re.findall(r'python', str(response.content))
+# sending data from page to Beautiful soup 
+soup = BeautifulSoup(response.content, 'html5lib')
 
-print(outp)
+# selecting ID data I pull
+results = soup.find(id='resultsCol')
 
-input()
+
+# pulling further actual jobs from the class
+jobs = results.find_all('div', class_='result')
+print('below is the len')
+print(len(jobs))
+
+# accessing a single element to get the title
+#title = jobs[0].find('h2')
+### pulling the link from the job 
+#title_link = title.find('a')
+# this will give you a partial, you need a full url"https.indeed "
+#print(title_link['href']) 
+
+### readable link / stripping extra html
+#link_text = title_link.text
+### clean it up
+#print(link_text.strip())
+# all together now
+job_titles = [job.find('h2').find('a').text.strip() for job in jobs]
+print(*job_titles, sep='\n')
+
+
+
+
+#out_put = re.findall(r'python', str(response.content))
+
+#print(out_put[1:8])
